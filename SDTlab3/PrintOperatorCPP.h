c62e188f18a8f526a26c6df5ec1da1c97ef36546
Alexander Loginov <kipling.live@gmail.com>
2024-06-19 03:40:05 +0700

    class diagram modified

--- SDTlab3/PrintOperatorCPP.h
+++ SDTlab3/PrintOperatorCPP.h
@@ -7,8 +7,6 @@ class PrintOperatorCPP :
 
 public:
 
-    void add(const std::shared_ptr<Unit>&, Flags = 0) {}
-
     explicit PrintOperatorCPP(const std::string& text) : m_text(text) {}
 
     std::string compile(unsigned int level = 0) const {
@@ -16,7 +14,7 @@ public:
     }
 
 private:
-
+    void add(const std::shared_ptr<Unit>&, Flags = 0) {}
     std::string m_text;
 };
 
--- SDTlab3/PrintOperatorJava.h
+++ SDTlab3/PrintOperatorJava.h
@@ -6,8 +6,6 @@ class PrintOperatorJava :
 {
 public:
 
-    void add(const std::shared_ptr<Unit>&, Flags = 0) {}
-
     explicit PrintOperatorJava(const std::string& text) : m_text(text) {}
 
     std::string compile(unsigned int level = 0) const {
@@ -15,7 +13,7 @@ public:
     }
 
 private:
-
+    void add(const std::shared_ptr<Unit>&, Flags = 0) {}
     std::string m_text;
 };
 
